64e8b2e238193ae341821bba17e83b3d76f29a34
cc <votaryofturms@icloud.com>
2021-12-15 01:51:47 -0500

    created files for components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h2>Client Auth Project</h2>
+      <Route>
+
+      </Route>
+      <Route>
+
+      </Route>
+      <Route>
+
+      </Route>
     </div>
   );
 }
