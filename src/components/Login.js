db8c5fb89cfb406fc4328571ef6a45e4acd96be4
cc <votaryofturms@icloud.com>
2021-12-15 01:59:31 -0500

    markup for Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,34 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
+
+const initialFormValues = {
+    username: '',
+    password: ''
+}
 
 
 
 const Login = () => {
+    const [formValues, setFormValues] = useState(initialFormValues)
 
     return (
         <>
             <form>
-
+                <label htmlFor='username'>Username&nbsp;
+                    <input
+                        id='username'
+                        name='username'
+                    />
+                </label>
+                <label htmlFor='password'>&nbsp;Password&nbsp;
+                    <input
+                        id='password'
+                        name='password'
+                        type='password'
+                    />
+                </label>
+                <button>Submit</button>
             </form>
         </>
     )
